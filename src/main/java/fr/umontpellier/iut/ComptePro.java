9a8f523b3a941624de0e7c539e12e13c5078d04c
David Binaud <david.binaud@gmail.com>
2019-09-06 13:49:09 +0200

    implementation classe ComptePro qui hérite de Compte; avec attribut et constructeur

---
+++ src/main/java/fr/umontpellier/iut/ComptePro.java
@@ -0,0 +1,11 @@
+package fr.umontpellier.iut;
+
+public class ComptePro extends Compte {
+
+    private String siren;
+
+    public ComptePro(double solde, String iban, String nomclient, String adresse, String siren) {
+        super(solde, iban, nomclient, adresse);
+        this.siren = siren;
+    }
+}
