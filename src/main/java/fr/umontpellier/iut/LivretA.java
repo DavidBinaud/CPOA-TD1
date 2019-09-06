d049aad08367419cd8207dbd0ce621f99d19d7aa
David Binaud <david.binaud@gmail.com>
2019-09-06 13:53:43 +0200

    implementation classe LivretA qui hérite de Livret; avec attribut et constructeur

---
+++ src/main/java/fr/umontpellier/iut/LivretA.java
@@ -0,0 +1,11 @@
+package fr.umontpellier.iut;
+
+public class LivretA extends Livret {
+
+    private double plafond;
+
+    public LivretA(double solde, String iban, String nomclient, String adresse, double tauxinteret, double plafond) {
+        super(solde, iban, nomclient, adresse, tauxinteret);
+        this.plafond = plafond;
+    }
+}
