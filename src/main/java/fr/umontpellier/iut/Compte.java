28daeb6de1180a4ff9de1904904fe08ac7dc038c
David Binaud <david.binaud@gmail.com>
2019-09-06 13:45:52 +0200

    implementation classe Compte avec attribut et constructeur

---
+++ src/main/java/fr/umontpellier/iut/Compte.java
@@ -0,0 +1,20 @@
+package fr.umontpellier.iut;
+
+public abstract class Compte{
+
+    private double solde;
+
+    private String iban;
+
+    private String nomclient;
+
+    private String adresse;
+
+    public Compte(double solde, String iban, String nomclient, String adresse) {
+        this.solde = solde;
+        this.iban = iban;
+        this.nomclient = nomclient;
+        this.adresse = adresse;
+    }
+
+}
