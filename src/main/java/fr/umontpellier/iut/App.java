9637c5a2052fee1123fcefd80d18d8b1e8ccae63
David Binaud <david.binaud@gmail.com>
2019-09-06 15:00:19 +0200

    App main, changement initialisation des objets en Compte

--- src/main/java/fr/umontpellier/iut/App.java
+++ src/main/java/fr/umontpellier/iut/App.java
@@ -3,16 +3,16 @@ package fr.umontpellier.iut;
 public class App {
     public static void main(String[] args) {
 
-        CompteCourant courant = new CompteCourant(3000,"FR76232313","Sarlin","Juvignac 150 rue de la bitch","120001546464");
+        Compte courant = new CompteCourant(3000,"FR76232313","Sarlin","Juvignac 150 rue de la bitch","120001546464");
 
 
-        ComptePro pro = new ComptePro(10000,"FR1310588","Binaud","75 rue des lavandes","siren1451");
+        Compte pro = new ComptePro(10000,"FR1310588","Binaud","75 rue des lavandes","siren1451");
 
-        LivretA livreta = new LivretA(650,"FR762115498","Bernadas","Clapiers avenue des pigeonniers",0.2,7500);
+        Compte livreta = new LivretA(650,"FR762115498","Bernadas","Clapiers avenue des pigeonniers",0.2,7500);
 
-        LivretPlusPlus livretplus = new LivretPlusPlus(1300,"FR15878423","Capell","derriere la fac de medecine",0.3,0.15);
+        Compte livretplus = new LivretPlusPlus(1300,"FR15878423","Capell","derriere la fac de medecine",0.3,0.15);
 
-        LivretSpeculation livretspec = new LivretSpeculation(2400,"FR224564651","Manelphe","idontknow",0.5,0.3);
+        Compte livretspec = new LivretSpeculation(2400,"FR224564651","Manelphe","idontknow",0.5,0.3);
 
         System.out.println(courant.toString());
         System.out.println(pro.toString());
