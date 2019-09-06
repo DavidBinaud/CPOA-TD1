2ab588a302d6292517e1c81aca1fff1f843681ae
David Binaud <david.binaud@gmail.com>
2019-09-06 13:50:31 +0200

    implementation classe Livret  qui hérite de Compte; avec attribut et constructeur

---
+++ src/main/java/fr/umontpellier/iut/Livret.java
@@ -0,0 +1,11 @@
+package fr.umontpellier.iut;
+
+public abstract class Livret extends Compte{
+
+    private double tauxinteret;
+
+    public Livret(double solde, String iban, String nomclient, String adresse, double tauxinteret) {
+        super(solde, iban, nomclient, adresse);
+        this.tauxinteret = tauxinteret;
+    }
+}
